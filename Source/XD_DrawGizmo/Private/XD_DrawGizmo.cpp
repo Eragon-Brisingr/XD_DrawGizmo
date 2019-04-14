f6bca4bb4189b27060c88643001abeaebfb5ea12
Eragon-Brisingr <450614754@qq.com>
2019-04-14 15:28:44 +0800

    更新函数

--- Source/XD_DrawGizmo/Private/XD_DrawGizmo.cpp
+++ Source/XD_DrawGizmo/Private/XD_DrawGizmo.cpp
@@ -10,7 +10,7 @@ void FXD_DrawGizmoModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	FEditorModeRegistry::Get().RegisterMode<FXD_DrawGizmoEdMode>(FXD_DrawGizmoEdMode::EM_XD_DrawGizmoEdModeId);
-	GEngine->OnPostEngineInit.AddLambda([]()
+	FCoreDelegates::OnPostEngineInit.AddLambda([]()
 		{
 			GLevelEditorModeTools().AddDefaultMode(FXD_DrawGizmoEdMode::EM_XD_DrawGizmoEdModeId);
 		});
@@ -21,7 +21,7 @@ void FXD_DrawGizmoModule::ShutdownModule()
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 	FEditorModeRegistry::Get().UnregisterMode(FXD_DrawGizmoEdMode::EM_XD_DrawGizmoEdModeId);
-	GEngine->OnPostEngineInit.RemoveAll(this);
+	FCoreDelegates::OnPostEngineInit.RemoveAll(this);
 }
 
 #undef LOCTEXT_NAMESPACE
